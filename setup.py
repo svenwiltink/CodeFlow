b0e2fc036825e8a587cec6741a986d3e9674edaa
sven <sven.wiltink@hotmail.com>
2015-12-30 22:34:33 +0100

    cleanup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='PyFlow',
+      version='0.0.2',
+      packages=['PyFlow'],
+      package_dir={'': 'src'}
+      )
--- src/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-
-setup(name='PyFlow',
-      version='0.1',
-      )
