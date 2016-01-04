c81a0e17a16635de5ebe9ed44924831cc0a29d2a
SvenWiltink <swiltink@transip.nl>
2016-01-05 00:33:40 +0100

    added a testcase for when the run function is not implemented

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -17,6 +17,10 @@ class NotAState:
         pass
 
 
+class NoRunMethodState(State):
+    pass
+
+
 class TestWorkFlow(unittest.TestCase):
 
     workflowFile = "tests/resources/workflow.json"
@@ -36,4 +40,9 @@ class TestWorkFlow(unittest.TestCase):
     def test_is_not_state(self):
         workflow = Workflow.loadFromFile("tests/resources/not_a_state_workflow.json")
         with self.assertRaises(TypeError):
+            workflow.run()
+
+    def test_no_run_method(self):
+        workflow = Workflow.loadFromFile("tests/resources/no_run_method_state_workflow.json")
+        with self.assertRaisesRegex(RuntimeError, "run function not implemented"):
             workflow.run()
